ed0cc7665abee52df2ba84dcbc7b0dc0d69e83f1
Idzel Jaimes <irjaimes@outlook.com>
2021-01-11 10:16:43 -0800

    updated jquery variable seclectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 
-var today = $("#currentDay");
-//var timeRow = $(".time-row");
-var schedule = $(".schedule");
+var $today = $("#currentDay");
+var $timeRow = $(".time-row");
+var $schedule = $(".schedule");
 var thingsToDo = []; //to store todo hour and text properties
 
-function runSchedule() {
+function initSchedule() {
+    console.log(thingsToDo)
     //for each time block set time & link object
-    $(".time-row").each(function () {
-        var dueTime = parseInt($(this).attr("data-hour")); //√√√√√
+    $timeRow.each(function () {
+        var $block = $(this);
+        var blockTime = parseInt($block.attr("data-hour")); //√√√√√
         //object to hold hour and txt input
         var toDoObj = {
-            hour: dueTime,
+            hour: blockTime,
             text: "",
         }
         //add toDo object to thingsToDo Array
@@ -25,42 +27,44 @@ function runSchedule() {
 var currentHour = moment().format("H");
 
 function blockTimeVsCurrentTime() { 
-    $(".time-row").each(function () {
-        var currentBlockHr = parseInt($(this).attr("data-hour")); // √√√√√
+    $timeRow.each(function () {
+        var $block = $(this)
+        var currentBlockHr = parseInt($block.attr("data-hour")); // √√√√√
 
         //update class for style depending on time of day
         if (currentBlockHr == currentHour) { //if timeRow hour is current time
-            $(this).addClass("present").removeClass("past future"); // √√√√√
+            $block.addClass("present").removeClass("past future"); // √√√√√
         }
         if (currentBlockHr < currentHour) { // if timeRow hour has passed
-            $(this).addClass("past").removeClass("present future"); // √√√√√
+            $block.addClass("past").removeClass("present future"); // √√√√√
         }
         if (currentBlockHr > currentHour) { //if timeRow hour is pending
-            $(this).addClass("future").removeClass("past present"); // √√√√√
+            $block.addClass("future").removeClass("past present"); // √√√√√
         }
     });
 }
 
 
-
+// function to render the todoitems from local storage
 function renderToDoItems() {
     thingsToDo = localStorage.getItem("ToDos");
     thingsToDo = JSON.parse(thingsToDo);
     //loop through thingsToDo array, assign text input to the block that matches hour
     for (var i = 0; i < thingsToDo.length; i++) {
-        var toDoItemHr = thingsToDo[i].hour;
+        var itemHr = thingsToDo[i].hour;
         var inputText = thingsToDo[i].text;
-    // display to do item 
-        $("[data-hour=" + toDoItemHr + "]").children("textarea").val(inputText); // √√√√√
+    // display to do item value in text area
+        $("[data-hour=" + itemHr + "]").children("textarea").val(inputText); // √√√√√
     }
+    console.log(thingsToDo)
 }
 
 // Event handler for Save Button     √√√√√
 function saveBtnHandler() {
-    var thisBlock = $(".time-row")
-    var checkHour = $(thisBlock).parent().attr("data-hour");
-console.log(thisBlock)
-    var addItem = (($(thisBlock).parent()).children("textarea")).val();
+    var $block = $(this)
+    var checkHour = $(this).parent().attr("data-hour");
+console.log(checkHour)
+    var addItem = (($(this).parent()).children("textarea")).val();
 
     //check to update to do item based on the hour of the button clicked
     for (var j = 0; j < thingsToDo.length; j++) {
@@ -83,16 +87,16 @@ $(document).ready(function () {
     //if there's nothing for the thingsToDo in local storage
     if (!localStorage.getItem("ToDos")) {
         //run objects array
-        runSchedule();
+        initSchedule();
     } 
     //display today's date
-    today.text(date);
+    $today.text(date);
 
     //render scheduled todo items from local storage
     renderToDoItems();
 
     //save new todo on save button click
-    schedule.on("click", "button", saveBtnHandler);
+    $schedule.on("click", "button", saveBtnHandler);
 
 });
 
